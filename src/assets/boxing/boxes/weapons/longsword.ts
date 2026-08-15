33c29004fcd095f0ec67e6c9b0b4e236d929f1c3
Michel Szinavel <michel.szinavel@gmail.com>
2026-08-15 19:55:24 -0400

    changing long sword model

--- src/assets/boxing/boxes/weapons/longsword.ts
+++ src/assets/boxing/boxes/weapons/longsword.ts
@@ -3,11 +3,11 @@ import { registerBoxParts } from '../../registry';
 registerBoxParts('longsword', [
   { size: [0.12, 0.4, 0.12], position: [0, -0.15, 0.05], color: 0x5c3a1e, metal: 0.1, rough: 0.9 },
   { size: [0.36, 0.1, 0.1], position: [0, 0.08, 0], color: 0x666666, metal: 0.7, rough: 0.35 },
-  { size: [0.1, 0.14, 1.15], position: [0, 0.12, -0.65], color: 0xb8c0cc, metal: 0.85, rough: 0.22 },
+  { size: [0.1, 0.22, 3.15], position: [0, 0.12, -1.25], color: 0xb8c0cc, metal: 0.85, rough: 0.22 },
 ]);
 
 registerBoxParts('longsword_upgraded', [
   { size: [0.12, 0.4, 0.12], position: [0, -0.15, 0.05], color: 0x9a6030, metal: 0.05, rough: 0.85 },
   { size: [0.36, 0.1, 0.1], position: [0, 0.08, 0], color: 0xd4a017, metal: 0.75, rough: 0.25, emissive: 0xd4a017, emissiveIntensity: 0.18 },
-  { size: [0.1, 0.14, 1.15], position: [0, 0.12, -0.65], color: 0xffd700, metal: 0.85, rough: 0.15, emissive: 0xffd700, emissiveIntensity: 0.3 },
+  { size: [0.1, 0.22, 3.15], position: [0, 0.12, -1.25], color: 0xffd700, metal: 0.85, rough: 0.15, emissive: 0xffd700, emissiveIntensity: 0.3 },
 ]);
